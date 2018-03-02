22e541d827fb16f36bff163bc76f174e22fdb88d
Manuel Garcia Rebollo <m.garcia.rebollo@mobgen.com>
2018-03-02 21:19:20 +0100

    hello world

---
+++ hello_world/README.md
@@ -0,0 +1,8 @@
+# hello_world
+
+hello world
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](https://flutter.io/).
---
+++ hello_world/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+// => One line functions or methods
+void main() => runApp(new MyApp());
+
+//Even app is a widget, in Flutter most everything is a Widget
+class MyApp extends StatelessWidget {
+  @override
+  //A widget’s main job is to provide a build() method
+  // that describes how to display the widget in terms of other, lower level widgets.
+  Widget build(BuildContext context) {
+    //Material, base Widget with Material design standard
+    return new MaterialApp(
+      title: 'Welcome to Flutter',
+      //Scaffold widget,  from the Material library, provides a default app bar,
+      // title, and a body property that holds the widget tree for the home screen.
+      // The widget subtree can be quite complex.
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Welcome to Flutter'),
+        ),
+        body: new Center(
+          child: new Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
