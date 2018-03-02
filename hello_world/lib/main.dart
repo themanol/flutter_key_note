7562553ac97cc6e797cc568a56514186583a52b4
Manuel Garcia Rebollo <m.garcia.rebollo@mobgen.com>
2018-03-02 23:53:35 +0100

    add list view

--- hello_world/lib/main.dart
+++ hello_world/lib/main.dart
@@ -16,14 +16,7 @@ class MyApp extends StatelessWidget {
       //Scaffold widget,  from the Material library, provides a default app bar,
       // title, and a body property that holds the widget tree for the home screen.
       // The widget subtree can be quite complex.
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text('Welcome to Flutter'),
-        ),
-        body: new Center(
-          child: new RandomWords(),
-        ),
-      ),
+      home: new RandomWords(),
     );
   }
 }
@@ -34,10 +27,56 @@ class RandomWords extends StatefulWidget {
 }
 
 class RandomWordsState extends State<RandomWords> {
+  //(_). Prefixing an identifier with an underscore
+  // enforces privacy in the Dart language.
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  Widget _buildRow(WordPair pair) {
+    return new ListTile(
+      title: new Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return new ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        // The itemBuilder callback is called, once per suggested word pairing,
+        // and places each suggestion into a ListTile row.
+        // For even rows, the function adds a ListTile row for the word pairing.
+        // For odd rows, the function adds a Divider widget to visually
+        // separate the entries. Note that the divider may be difficult
+        // to see on smaller devices.
+        itemBuilder: (context, i) {
+          // Add a one-pixel-high divider widget before each row in theListView.
+          if (i.isOdd) return new Divider();
+
+          // The syntax "i ~/ 2" divides i by 2 and returns an integer result.
+          // For example: 1, 2, 3, 4, 5 becomes 0, 1, 1, 2, 2.
+          // This calculates the actual number of word pairings in the ListView,
+          // minus the divider widgets.
+          final index = i ~/ 2;
+          // If you've reached the end of the available word pairings...
+          if (index >= _suggestions.length) {
+            // ...then generate 10 more and add them to the suggestions list.
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        }
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    final wordPair = new WordPair.random();
-    return new Text(wordPair.asPascalCase);
+    return new Scaffold (
+      appBar: new AppBar(
+        title: new Text('Startup Name Generator'),
+      ),
+      body: _buildSuggestions(),
+    );
   }
 
 }
